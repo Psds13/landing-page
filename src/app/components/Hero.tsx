fc6c720366a597f43b3baaed699354bfa3dbc5e0
Psds13 <edmilsonsegundo15@gmail.com>
2025-04-07 15:59:20 -0300

    Arquivos atualizados

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,31 +1,39 @@
 const Hero = () => {
   return (
-    <section id="hero" className="bg-gray-900 text-white h-screen flex items-center justify-center">
+    <section id="hero" className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
       <div className="text-center px-5">
-      <img src="/eusinho.jpg" alt="Minha foto" width={400} height={400}
-      className="w-[300px] h-[300px] rounded-full mx-auto mb-4 object-cover" />
-
+        <img
+          src="/eusinho.jpg"
+          alt="Minha foto"
+          width={400}
+          height={400}
+          className="w-[300px] h-[300px] rounded-full mx-auto mb-4 object-cover"
+        />
 
         <h1 className="text-5xl font-bold mb-4">Olá, eu sou Edmilson Oliveira</h1>
         <p className="text-xl mb-6">
           Desenvolvedor Web apaixonado por criar interfaces incríveis e experiências digitais impactantes.
         </p>
-        <a
-          href="#projects"
-          className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-800 transition"
-        >
-          Veja meus projetos
-        </a>
-        <a
-          href="docs/Edmilson_Oliveira.pdf"
-          download
-          className="bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-800 transition ml-4"
-        >
-          Baixar Currículo
-        </a>
+
+        {/* Botões dentro de um contêiner flexível e responsivo */}
+        <div className="flex flex-wrap justify-center gap-4">
+          <a
+            href="#projects"
+            className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-800 transition"
+          >
+            Veja meus projetos
+          </a>
+          <a
+            href="/Edmilson_Oliveira.pdf"
+            download
+            className="bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-800 transition"
+          >
+            Baixar Currículo
+          </a>
+        </div>
       </div>
     </section>
   );
 };
-export default Hero;
 
+export default Hero;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/app/components/Footer";
 import Header from "@/app/components/Header";
 import "./globals.css";
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,8 +10,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Landing Page</title>
+        <title>Solução de tecnologia</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/ynnothivix.jpg" type="img/jpg" />
+        {/* ou, se estiver usando png: type="image/png" */}
       </head>
       <body>
         <Header />
